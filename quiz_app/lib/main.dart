fc97068eef57422608eefc48f0dc1782903fef42
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-10-10 06:48:33 -0300

    Aula 27: Building a Widget Tree.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -4,7 +4,16 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App',
+          style: TextStyle(color: Colors.black),),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Text('This is my default text'),
+      ),
+    );
   }
 }
