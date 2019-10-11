41236569623118d6be8d5b54d4d0ad1d017ba0f5
Matheus Estanislau <matheus.estanislau7@gmail.com>
2019-10-11 07:22:04 -0300

    Aula 34: Updating Correctly with stateful Widgets.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
   void answerQuestion() {
-    print("Answer 1 chosen!");
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
-  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -21,21 +37,20 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('The Question'),
+              Text(questions[questionIndex]),
               RaisedButton(
-                child: Text('Answer 1'), 
+                child: Text('Answer 1'),
                 onPressed: answerQuestion,
               ),
               RaisedButton(
-                child: Text('Answer 2'), 
+                child: Text('Answer 2'),
                 onPressed: () => print("Answer 2 chosen!"),
               ),
               RaisedButton(
-                child: Text('Answer 3'), 
-                onPressed: () {
-                  print('Answer 3 chosen!');
-                }
-                ),
+                  child: Text('Answer 3'),
+                  onPressed: () {
+                    print('Answer 3 chosen!');
+                  }),
             ],
           )),
     );
